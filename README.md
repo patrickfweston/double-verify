31af20552e3eae72863b3ac326b1eaac80fb1bf9
patrickfweston <patrickfweston@gmail.com>
2016-10-28 09:39:51 -0400

    Setting up gulp http server ⚡️

---
+++ README.md
@@ -0,0 +1,9 @@
+# Double Verify
+
+A simple prototyped page utilizing material design
+
+#### To Install
+1. Clone this repository
+1. From the project root, run `npm install --save-dev gulp-connect gulp-sass`
+1. After installing, run `gulp`
+1. The project will be served up from http://localhost:8080
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass');
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+  });
+});
+
+gulp.task('sass', function() {
+  gulp.src('sass/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('sass/*.sass', ['sass']);
+})
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
