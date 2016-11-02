101470421ba552b3adafa1eeef241bd54be06a0b
patrickfweston <patrickfweston@gmail.com>
2016-11-01 22:00:19 -0500

    Few more finishing touches

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
-$('.form--search__reveal').on('click', function() {
-  $('.form--search').addClass('is-visible');
-  $(this).addClass('is-hidden');
+$(document).on('ready', function() {
+  $('.form--search__reveal').on('click', function() {
+    $('.form--search').addClass('is-visible');
+    $(this).addClass('is-hidden');
+  });
+
+  setRailHeight();
 });
+
+$(window).on('load', function() {
+  setRailHeight();
+});
+
+$(window).on('resize', function() {
+  setRailHeight();
+});
+
+function setRailHeight() {
+  if ($(window).width() > 960) {
+    var $mainColumn = $('.l-container').find('.card').last();
+    var $railColumn = $('.l-overview');
+
+    var mainBottom = $mainColumn.outerHeight() + $mainColumn.offset().top;
+    var railBottom = $railColumn.outerHeight() + $railColumn.offset().top;
+
+    var railHeight = $railColumn.height();
+
+    $railColumn.height((mainBottom - railBottom) + railHeight);
+  } else {
+    $('.l-overview').removeAttr('style');
+  }
+}
